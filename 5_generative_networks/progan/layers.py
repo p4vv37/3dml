f41b3c212f873f23f39a02026814a2b5c45c9ca0
pawel <p4vv37@gmail.com>
2020-06-19 20:45:09 +0200

    Fixed equalized learning scale

--- 5_generative_networks/progan/layers.py
+++ 5_generative_networks/progan/layers.py
@@ -106,20 +106,19 @@ class WeightedSum(Add):
 
 
 class EqualizedConv2D(Conv2D):
-    def __init__(self, filters, kernel, *args, **kwargs):
-        self.scale = None
-        super(EqualizedConv2D, self).__init__(filters, kernel, *args, **kwargs)
+    """   Based on https://github.com/MSC-BUAA/Keras-progressive_growing_of_gans/issues/4   """
+    def __init__(self, *args, **kwargs):
+        self.scale = 1
+        super(EqualizedConv2D, self).__init__(*args, **kwargs)
 
     def build(self, input_shape):
-        fan_in = np.prod(input_shape[1:])
-        self.scale = np.sqrt(fan_in/2)
-        self.scale **= 0.2
-        # print(self.scale)
-        return super(EqualizedConv2D, self).build(input_shape)
+        super().build(input_shape)
+        kernel_shape = backend.int_shape(self.kernel)
+        std = np.sqrt(2) / np.sqrt(np.prod(kernel_shape[:-1]))
+        self.scale = backend.constant(std, dtype=backend.floatx())
 
-    def call(self, inputs):
-        if self.scale is not None:
-            weights = self.get_weights()
-            weights = np.divide(weights, self.scale)
-            self.set_weights(weights)
-        return super(EqualizedConv2D, self).call(inputs)
+    def call(self, inputs, **kwargs):
+        k = self.kernel
+        self.kernel = self.kernel * self.scale
+        x = super().call(inputs)
+        return x
